26a0a9a4306b185f00b6e73c123842257cf6ec95
Dalton Huynh <dalton.huynh@livingsocial.com>
2012-01-03 12:26:23 -0500

    support for rails 2.3.8 and reset changed_attributes for exception attributes

--- lib/deep_cloneable.rb
+++ lib/deep_cloneable.rb
@@ -1,5 +1,9 @@
 class ActiveRecord::Base
   module DeepCloneable
+    def self.included(base)
+      base.alias_method_chain :clone, :deep_cloning
+    end
+
     # clones an ActiveRecord model. 
     # if passed the :include option, it will deep clone the given associations
     # if passed the :except option, it won't clone the given attributes
@@ -40,16 +44,16 @@ class ActiveRecord::Base
     # ==== Cloning a model without an attribute or nested multiple attributes   
     #    pirate.clone :include => :parrot, :except => [:name, { :parrot => [:name] }]
     # 
-    def clone(options = {})
+    def clone_with_deep_cloning(options = {})
       dict = options[:dictionary]
       dict ||= {} if options.delete(:use_dictionary)
       
       kopy = unless dict
-        super()
+        clone_without_deep_cloning
       else
         tableized_class = self.class.name.tableize.to_sym
         dict[tableized_class] ||= {}
-        dict[tableized_class][self] ||= super()
+        dict[tableized_class][self] ||= clone_without_deep_cloning
       end
 
       deep_exceptions = {}
@@ -59,22 +63,23 @@ class ActiveRecord::Base
           kopy.send(:write_attribute, attribute, attributes_from_column_definition[attribute.to_s]) unless attribute.kind_of?(Hash)
         end
         deep_exceptions = exceptions.select{|e| e.kind_of?(Hash) }.inject({}){|m,h| m.merge(h) }
+        kopy.instance_variable_set(:@changed_attributes, {})
       end
-    
+
       if options[:include]
         Array(options[:include]).each do |association, deep_associations|
           if (association.kind_of? Hash)
             deep_associations = association[association.keys.first]
             association = association.keys.first
           end
-          
+
           opts = deep_associations.blank? ? {} : {:include => deep_associations}
           opts.merge!(:except => deep_exceptions[association]) if deep_exceptions[association]
           opts.merge!(:dictionary => dict) if dict
-        
+
           association_reflection = self.class.reflect_on_association(association)
           raise AssociationNotFoundException.new("#{self.class}##{association}") if association_reflection.nil?
-                    
+
           cloned_object = case association_reflection.macro
             when :belongs_to, :has_one
               self.send(association) && self.send(association).clone(opts)
@@ -96,9 +101,9 @@ class ActiveRecord::Base
 
       return kopy
     end
-    
+
     class AssociationNotFoundException < StandardError; end    
   end
-  
+
   include DeepCloneable
 end
